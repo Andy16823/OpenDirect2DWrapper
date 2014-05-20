82b5e336e223d08b7603b4a7cf928b8c61b69112
Andy16823 <wagnerandreas17@yahoo.de>
2014-05-20 21:07:30 +0200

    Create dwRectangle.cpp

---
+++ Versions/Release-1.0.0.0/Direct2DWrapper/dwRectangle.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "dwRectangle.h"
+
+
+namespace Direct2DWrapper
+{
+	dwRectangle::dwRectangle(float X, float Y, float Width, float Height)
+	{
+		this->X = X;
+		this->Y = Y;
+		this->Width = Width;
+		this->Height = Height;
+	}
+
+	dwRectangle::dwRectangle()
+	{
+
+	}
+
+	dwRectangle::~dwRectangle(void)
+	{
+
+	}
+
+	void Direct2DWrapper::dwRectangle::Initial()
+	{
+
+	}
+
+}
