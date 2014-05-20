fddaeb900f046612312a090947218fa74e5055dc
Andy16823 <wagnerandreas17@yahoo.de>
2014-05-20 20:57:34 +0200

    Create stdafx.cpp

---
+++ Versions/Release-1.0.0.0/Direct2DWrapper/stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
+// Direct2DWrapper.pch ist der vorkompilierte Header.
+// stdafx.obj enthält die vorkompilierten Typinformationen.
+
+#include "stdafx.h"
